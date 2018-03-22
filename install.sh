7a6a0581a3c2830f7cfb4a94c099387ac7b9ad1d
Samuel <samuel@localhost.localdomain>
2018-03-22 16:41:17 +0800

    Added ascii2img installation Added uninstallation script

--- install.sh
+++ install.sh
@@ -3,3 +3,10 @@ sudo cp ./urlencode/urlencode.py /usr/bin/
 
 echo "Copying urldecode.py..."
 sudo cp ./urldecode/urldecode.py /usr/bin/
+
+echo "Copying clearExtraLines.py..."
+sudo cp ./clearExtraLines/clearExtraLines.py /usr/bin/
+
+echo "Copying ascii2img.py..."
+sudo cp ./ascii2img/ascii2img /usr/bin/
+sudo cp -r ./ascii2img/src /usr/bin/ascii2img.d
---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+echo "Deleting urlencode.py..."
+sudo rm /usr/bin/urlencode.py
+
+echo "Deleting urldecode.py..."
+sudo rm /usr/bin/urldecode.py
+
+echo "Deleting clearExtraLines.py..."
+sudo rm /usr/bin/clearExtraLines.py
+
+echo "Deleting ascii2img.py..."
+sudo rm /usr/bin/ascii2img
+sudo rm -r /usr/bin/ascii2img.d
