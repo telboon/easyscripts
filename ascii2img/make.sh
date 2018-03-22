915b70455d3587e72e72e69066b09c53fc79f480
Samuel <samuel@localhost.localdomain>
2018-03-22 18:44:33 +0800

    Updated ascii2img to be cross python3

--- ascii2img/make.sh
+++ ascii2img/make.sh
@@ -1,1 +1,5 @@
-pex . -c __main__.py -o ascii2img
+pex --python=python3 . -c __main__.py -o ascii2img
+echo '#!/usr/bin/env python3' | cat - ascii2img > ascii2img.new
+rm ascii2img
+mv ascii2img.new ascii2img
+chmod +x ascii2img
